98a1e4655f56470210c835aaaf3f483acc8d329b
Yun-Ju Wu <yw893@cornell.edu>
2017-11-11 13:10:44 -0500

    update the index with nav bar and header

--- Final Project/index.php
+++ Final Project/index.php
@@ -1,14 +1,10 @@
-<?php  
-?>
-
-
-
-
-
 <!DOCTYPE html>
 <html>
-<header>
-  <body>
+  <?php include("includes/header.php"); ?>
+  <body id = "Home">
+
+    <?php include("includes/navigation.php"); ?>
+    <cite id = "cover_cite">Cover Photo Taken by me and Logo designed by ZheWei Wu</cite>
+
   </body>
-</header>
 </html>
