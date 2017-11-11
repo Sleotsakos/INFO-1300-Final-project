c1f54bb8efe8690b921a21f3d5222672cdb055f9
Yun-Ju Wu <yw893@cornell.edu>
2017-11-11 13:25:04 -0500

    add in includes

---
+++ Final Project/includes/navigation.php
@@ -0,0 +1,12 @@
+<div id="nav">
+  <nav>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">For Students</a></li>
+      <li><a href="">For Sponsors</a></li>
+      <li><a href="">Members</a></li>
+      <li><a heref="">Contact</a><li>
+    </ul>
+  </nav>
+</div>
