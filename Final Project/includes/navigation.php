11bdfb28244ced210fcb7bfeeac93c60e38163a0
Socrate <sgl55@cornell.edu>
2017-11-13 08:39:00 -0500

    sponsors page

--- Final Project/includes/navigation.php
+++ Final Project/includes/navigation.php
@@ -4,9 +4,9 @@
     <ul>
       <li><a href="index.php">Home</a></li>
       <li><a href="about.php">About</a></li>
-      <li><a href="forstudents.php">For Students</a></li>
-      <li><a href="forsponsors.php">For Sponsors</a></li>
-      <li><a href="awards.php">Awards</a></li>
+      <!-- <li><a href="forstudents.php">For Students</a></li> -->
+      <li><a href="sponsors.php">Sponsors</a></li>
+      <!-- <li><a href="awards.php">Awards</a></li> -->
       <li><a href="members.php">Members</a></li>
       <li><a href="contact.php">Contact</a><li>
     </ul>
