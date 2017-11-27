54df120f0f607ae849c4f857b9c5dfa6845941f1
Jack Jones <jackjones@dhcp-rhodes-2042.eduroam.cornell.edu>
2017-11-27 17:33:19 -0500

    everything

---
+++ Final Project/schedule.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <?php include("includes/header.php"); ?>
+  <body id = "forsponsors">
+    <header class="header">
+      <div class="csscontainer1">
+        <img src="images/teamlogo.png" alt="teamlogo" id="teamlogo">
+        <h1 class="title">Schedule</h1>
+        <img src="images/logo.png" alt="logo" id="logo">
+      </div>
+    </header>
+    <?php include("includes/navigation.php"); ?>
+    <div class="schedulecontainer">
+      <img src="images/Timeline1.jpg" alt="Schedule" id="sched1">
+      <img src="images/Timeline2.jpg" alt="Schedule" id="sched2">
+    </div>
+  </body>
+</html>
