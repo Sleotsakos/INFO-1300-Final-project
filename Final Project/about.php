22d4f04e556627ef1a28b41735bb5ad440ffae43
Yun-Ju Wu <yw893@cornell.edu>
2017-11-27 17:34:52 -0500

    changes made to about page

--- Final Project/about.php
+++ Final Project/about.php
@@ -14,19 +14,41 @@
 
     <!-- Beginning of page content -->
     <div class="aboutcontainer">
-      <h1 class="contenttitle" id="theanime">The Anime</h1>
-      <video  id="animetrailer" controls>
-        <source src="media/anime_trailer.mp4" type="video/mp4">
-          RoboMasters Anime Trailer
-      </video>
-      <h1 class="contenttitle" id="howplayed">How it's Played</h1>
+      <div class="howplayed">
+        <h1 class="contenttitle" >About the Competition</h1>
+        <p>Hope to learn about the game rules check out this video!</p>
+      </div>
       <video  id="rulesvideo" controls>
         <source src="media/rules.mp4" type="video/mp4">
         The Rules
       </video>
+      <div id="theanime">
+        <h1 class="contenttitle" >Our Story: Animation Series</h1>
+        <p>
+          We have a animation series depicting out lives!<br/>
+          Check it out to know more.<br/>
+        </p>
+      </div>
+
+      <video  id="animetrailer" controls>
+        <source src="media/anime_trailer.mp4" type="video/mp4">
+          RoboMasters Anime Trailer
+      </video>
+      <!--link to videos from HKUST Robmaster Team -->
+      <a href="http://bestanime.top/watch/robomasters-the-animated-series-episode-001.html?s=6&r="
+      id ="animation_link" target="_blank">
+        Go watch the full episodes of our animation
+      </a>
+
+
       <h1 class="contenttitle" id="whatwedo">What We Do</h1>
-        <p id="dodescription"> "(Description of what team does to prepare for competitions,
-          build robots,...etc.)" </p>
+        <p id="dodescription">
+          Building up competition robots from scratch. <br/>
+          We are an advanced robotics team in Hong Kong University of Science
+          and Technolgy <br/>
+          With all members from software, mechanic, to electrical engineer,<br/>
+          we build up efficent and competitive robots and unbreakable friendship.
+         </p>
       <div id="slideshow1">
         <div id="image_container1">
           <!--images taken by RoboMasters HKUST-->
@@ -35,6 +57,9 @@
         <div id="button_container1">
             <button class="slide_button" id="left1" >Previous</button>
             <button class="slide_button" id="right1" >Next</button>
+        </div> <!-- end of button_container div -->
+      </div> <!--End Slideshow div -->
+    
         </div> <!-- end of button_container div -->
       </div> <!--End Slideshow div -->
 
