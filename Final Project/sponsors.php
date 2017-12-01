13934bd1ad1199ef9a6b6e249b0e977e446aad1c
Socrate <sgl55@cornell.edu>
2017-11-30 22:49:24 -0500

    competition sponsors

--- Final Project/sponsors.php
+++ Final Project/sponsors.php
@@ -31,5 +31,35 @@
         for details of what they hope to convey and express themselves)
       </p> -->
     </div>
+    <div class="sponsorscontainer3">
+      <h1 class="contenttitle">Competition Sponsors </h1>
+    </div>
+    <div class="sponsorscontainer4">
+      <!-- Image referenced from https://www.avnet.com -->
+      <img src="images/xilinx_logo.png" alt="xilinx_logo" id="xilinxlogo">
+      <p id="xilinxdescription">
+      Xilinx, Inc. is an American technology company, primarily a supplier of programmable logic devices. It is known for inventing the field-programmable gate array and as the first semiconductor company with a fabless manufacturing model.
+      </p>
+      <!-- Image referenced from https://i.ytimg.com/vi/VcC9ugakAmY/maxresdefault.jpg -->
+      <img src="images/nvidia_logo.jpg" alt="nvidia_logo" id="nvidialogo">
+      <p id="nvidiadescription">
+      Nvidia is an American technology company based in Santa Clara, California. It designs graphics processing units (GPUs) for the gaming, cryptocurrency, and professional markets, as well as system on a chip units (SoCs) for the mobile computing and automotive market.
+      </p>
+      <!-- Image referenced from https://fthmb.tqn.com -->
+      <img src="images/WD_logo.png" alt="WD_logo" id="WDlogo">
+      <p id="WDdescription">
+      Western Digital Corporation is an American computer data storage company and one of the largest computer hard disk drive manufacturers in the world, along with its main competitor Seagate Technology.
+      </p>
+      <!-- Image referenced from http://www.fresherjobs9.com/wp-content/uploads/2016/06/SanDisk_Logo_Wide.jpg-->
+      <img src="images/SanDisk_Logo.jpg" alt="SanDisk_logo" id="SanDisklogo">
+      <p id="SanDiskdescription">
+      SanDisk is an Israeli-American manufacturer of flash memory products, including memory cards and readers, USB flash drives, and solid state drives. As of February 2015, it is the third-largest manufacturer of flash memory.
+      </p>
+      <!-- Image referenced from https://www.gadgetplus.com.my-->
+      <img src="images/oppo_logo.jpg" alt="oppo_logo" id="oppologo">
+      <p id="oppodescription">
+      OPPO Electronics Corp. is a Chinese consumer electronics firm based in Guangdong. OPPO's major product lines include smartphones, Blu-ray players and other electronic devices.
+      </p>
+    </div>
   </body>
 </html>
